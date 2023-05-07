f1f6d38948a8e3f101da5e66aefe253e50d5639a
VARBASH19302 <varbash1419@gmail.com>
2023-05-07 19:30:29 +0530

    h

--- FingerCounter.py
+++ FingerCounter.py
@@ -3,8 +3,10 @@ import time
 import os
 import HandTrackingModule as htm
 
+#width height of cam
 wCam, hCam = 640, 480
 
+#turn on tthe camera device number 0/1
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
